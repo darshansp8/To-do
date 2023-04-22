5a21e13b106ac5cd7e16a0568acfe21ac300fcd8
Darshan Sakpal <d.sakpal38@gmail.com>
2023-04-23 00:22:52 +0100

    Integrated db and template

--- to_do/tasks/views.py
+++ to_do/tasks/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Tasks, Users
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    tasks = Tasks.objects.all()
+    print(tasks)
+    user = Users.objects.get(name="Darshan")
+    return render(request, 'index.html', {'tasks': tasks, 'user': user})
 

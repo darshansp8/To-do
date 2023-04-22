675491cbbce2b24917e63e08a4aa11426fa2b3f5
Darshan Sakpal <d.sakpal38@gmail.com>
2023-04-22 23:32:13 +0100

    Created Users and Tasks models

--- to_do/tasks/admin.py
+++ to_do/tasks/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Users, Tasks
 # Register your models here.
+
+models = [Users, Tasks]
+admin.site.register(models)
--- to_do/tasks/models.py
+++ to_do/tasks/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from datetime import date
 # Create your models here.
+
+
+class Users(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+
+
+class Tasks(models.Model):
+    email = models.ForeignKey(Users, on_delete=models.CASCADE)
+    task_desc = models.CharField(max_length=300)
+    task_status = models.BooleanField(default=False)
+    task_created = models.DateField(default=date.today)
+
+
+
